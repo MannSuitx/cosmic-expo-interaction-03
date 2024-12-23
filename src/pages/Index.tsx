c3622ba17ea102e778553cdc1c0001dce8a6f029
MannSuitx <146221794+MannSuitx@users.noreply.github.com>
2024-12-24 00:02:31 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,10 +2,11 @@ import Hero from "@/components/Hero";
 import CategoryCards from "@/components/CategoryCards";
 import RegisteredStalls from "@/components/RegisteredStalls";
 import RegistrationForm from "@/components/RegistrationForm";
-
+import Navbar from "@/components/Navbar";
 const Index = () => {
   return (
     <main className="min-h-screen bg-expo-black">
+      <Navbar />
       <Hero />
       <CategoryCards />
       <RegisteredStalls />
